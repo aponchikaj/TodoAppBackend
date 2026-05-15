e9c88c4296dde2759562b575212360f0e63455ca
aponchikaj <lazzexa@gmail.com>
2026-05-15 13:08:22 +0400

    fixed bugs

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -3,6 +3,7 @@ import USERS from '../../models/user.model.js'
 import newError from '../../services/newError.service.js'
 import checkAuth from '../../middlewares/auth.middleware.js'
 import bcrypt from 'bcryptjs'
+import TODOS from '../../models/todo.model.js'
 
 const router = express.Router()
 const FileLocation = 'src/routes/user/user.js'
@@ -21,12 +22,18 @@ router.get('/me',checkAuth,async(req,res)=>{
 })
 
 router.put('/password',checkAuth,async(req,res)=>{
-    const {password} = req.body;
+    const {password,oldPassword} = req.body;
 
     const passwordRegex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     if(!password || !passwordRegex.test(password)) return res.send({Success:false,Message:"Invalid Password."})
 
     try{
+
+        const USER = await USERS.findOne({_id:req.user._id});
+        const compare = await bcrypt.compare(oldPassword,USER.password);
+
+        if(!compare) return res.send({Success:false,Message:"Invalid old password."})
+
         const hashedPassword = await bcrypt.hash(password,12)
         await USERS.findByIdAndUpdate(req.user._id,{password:hashedPassword});
 
@@ -43,10 +50,11 @@ router.delete('/me',checkAuth,async(req,res)=>{
         const USER = await USERS.findOne({_id:req.user._id});
         if(!USER) return res.send({Success:false});
 
+        await TODOS.deleteMany({userID:USER._id})
+
         await USERS.findOneAndDelete({_id:req.user._id});
-        
+
         return res.send({Success:true,Message:"Deleted."})
-        
     }catch(e){
         await newError(e,FileLocation)
         return res.send({Success:false,Message:"Server error."})
